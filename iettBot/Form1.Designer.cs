fbfca522f6df1c6f9b2f8235530e48f6b70c03e6
kurtonur <kurt_onur@outlook.com.tr>
2016-12-14 01:16:42 +0300

    Xml dosyalama tamamlandı.

---
+++ iettBot/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace iettBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tmhatlar = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.tmhatlar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(660, 450);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(6, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(648, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hatlar XML Kayıt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tmhatlar
+            // 
+            this.tmhatlar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tmhatlar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tmhatlar.FormattingEnabled = true;
+            this.tmhatlar.ItemHeight = 16;
+            this.tmhatlar.Location = new System.Drawing.Point(6, 19);
+            this.tmhatlar.Name = "tmhatlar";
+            this.tmhatlar.Size = new System.Drawing.Size(648, 340);
+            this.tmhatlar.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(6, 411);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(648, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Duraklar XML Kayıt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(18, 468);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(648, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 497);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "IETT";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox tmhatlar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ iettBot/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NSoup;
+using NSoup.Nodes;
+using System.Threading;
+using System.Xml;
+
+namespace iettBot
+{
+    public partial class Form1 : Form
+    {
+        int i = 1;
+        int durum = 1;
+        List<string> dk;
+        public Form1()
+        {
+            InitializeComponent();
+
+            dk = new List<string>();
+            hatlar();
+            progressBar1.Maximum = i;
+            progressBar1.Minimum = 0;
+            
+        }
+
+        private void hatlar()
+        {
+            IConnection connection = NSoupClient.Connect("http://www.iett.gov.tr/tr/main/hatlar");
+            connection.Timeout(30000);
+            Document document = connection.Get();
+            foreach (Element Hat in document.Select("h4.DetailLi_name"))
+            {
+                int index = Hat.Text().IndexOf(' ');
+                string hatNo = Hat.Text().Substring(0, index).Trim();
+                string hatAdi = Hat.Text().Substring(index).Trim();
+                tmhatlar.Items.Add(i + "*" + hatNo + "*" + hatAdi);
+                i++;
+            }
+        }
+        
+        private void duraklar()
+        {
+
+            XmlTextWriter yaz = new XmlTextWriter("Duraklar.xml", System.Text.UTF8Encoding.UTF8);
+            yaz.Formatting = Formatting.Indented;
+            yaz.WriteStartDocument();
+            yaz.WriteStartElement("hepsi");
+
+            foreach (string item in tmhatlar.Items)
+            {
+            string[] ayir = item.Split('*');
+
+            IConnection connection;
+
+                connection = NSoupClient.Connect("http://www.iett.gov.tr/tr/main/hatlar/" + ayir[1]);
+
+            connection.Timeout(600000);
+
+            Document document = connection.Get();
+            
+            yaz.WriteStartElement("hatid");
+            yaz.WriteAttributeString("h",ayir[1]);
+            foreach (Element yon in document.Select("div.LineMapList ol"))
+            {
+                int i = 1;
+                if (yon.Select("li").Count > 0)
+                {
+                    yaz.WriteStartElement("yon");
+                    string yn = "Geliş";
+                    if (yon.Attr("data-station-direction") == "Going") yn = "Gidiş";
+                    yaz.WriteAttributeString("y", yn);
+                }
+                foreach (Element Durak in yon.Select("li"))
+                {
+
+
+                    yaz.WriteStartElement("durak");
+                    yaz.WriteAttributeString("durakno", i.ToString());
+                    yaz.WriteAttributeString("lat", Durak.Attr("data-station-lat"));
+                    yaz.WriteAttributeString("lng", Durak.Attr("data-station-lng"));
+                    yaz.WriteAttributeString("isim", Durak.Attr("data-station-name"));
+
+                    yaz.WriteEndElement();
+
+
+
+
+                    i++;
+                }
+                 if (yon.Select("li").Count > 0)
+                yaz.WriteEndElement();
+
+            }
+            yaz.WriteEndElement();
+            durum++;
+            }
+
+            yaz.WriteEndElement();
+            yaz.Close();
+            MessageBox.Show("Duraklar XML kayit edildi.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(duraklar));
+            thread.Start();
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = durum;
+            this.Text = durum.ToString();
+            if (progressBar1.Value == progressBar1.Maximum)
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                XmlTextWriter yaz = new XmlTextWriter("Hatlar.xml", System.Text.UTF8Encoding.UTF8);
+                yaz.Formatting = Formatting.Indented;
+                yaz.WriteStartDocument();
+                yaz.WriteStartElement("hepsi");
+                foreach (string item in tmhatlar.Items)
+                {
+                    string[] ayir = item.Split('*');
+                    yaz.WriteStartElement("hat");
+                    yaz.WriteAttributeString("id", ayir[0]);
+                    yaz.WriteAttributeString("numara", ayir[1]);
+                    yaz.WriteAttributeString("isim", ayir[2]);
+                    yaz.WriteEndElement();    
+                }
+                yaz.WriteEndElement();
+                yaz.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                
+                MessageBox.Show("Hatlar.xml Kayit Edildi.");
+            }
+        }
+    }
+}
